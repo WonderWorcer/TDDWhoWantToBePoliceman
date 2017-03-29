04b27789b68a2052fd72ff4d1c067305734281b3
WonderWorcer <aksromik@gmail.com>
2017-03-29 14:33:51 +0700

    first test

---
+++ src/tdd/Questions.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tdd;
+
+/**
+ *
+ * @author Роман
+ */
+public class Questions {
+
+
+    public Questions() {
+
+    }
+
+    public boolean newQuestion(String quest, int answer) {
+            return true;
+ }
+
+}
---
+++ test/tdd/QuestionsTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tdd;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Роман
+ */
+public class QuestionsTest {
+    
+    public QuestionsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+
+    }
+    
+    @Test
+    public void newQuestionTest(){
+        Questions question = new Questions();
+        boolean answer;
+        answer = question.newQuestion("Статья 322 уголовного кодекса",1);
+        assertTrue(answer);
+    }
+}
